bbae50b133f14a83d0c1b6d3c1a09f0f5df462c9
MatheusCarvalho-creat <matheusmrc7@gmail.com>
2025-05-11 09:47:33 -0300

    Criando rotas para o banco de dados

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,15 @@ router.get('/', function(req, res) {
 });
 
 module.exports = router;
+
+ const router = express.Router();
+ const mysql = require('mysql2/promise');
+ /* GET home page. */
+ router.get('/', function (req, res, next) {
+ mysql.createConnection({host: 'localhost',user: 'root',password: '',
+ database: 'backend-ii-express-mysql',port: 3306
+    }).then((connection) => {connection.query('SELECT * FROM clientes;')
+                .then((result) => {res.send(result[0]);});
+        });
+ });
+ module.exports = router;
